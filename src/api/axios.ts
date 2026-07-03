23f22a74d1dfe7ec4433a8f128d5427d060e86d3
olamide <olamxii@gmail.com>
2026-07-03 23:09:05 +0300

    Axios config

---
+++ src/api/axios.ts
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+const axiosClient = axios.create({
+  baseURL: import.meta.env.VITE_API_URL ?? "https://api.escuelajs.co/api/v1",
+
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
+
+axiosClient.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("ACCESS_TOKEN");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
+
+axiosClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("ACCESS_TOKEN");
+    }
+    return Promise.reject(error);
+  },
+);
+
+export default axiosClient;
