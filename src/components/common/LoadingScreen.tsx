7375cc032247d05c22bfc9319d641a8c9492c909
olamind <olamxii@gmail.com>
2026-07-06 15:07:43 +0300

    coomon(loading): width and heigth of the loading increased

--- src/components/common/LoadingScreen.tsx
+++ src/components/common/LoadingScreen.tsx
@@ -2,7 +2,7 @@
 const LoadingScreen = () => {
   return (
     <div className="min-h-screen bg-slate-50 flex flex-col justify-center items-center">
-      <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-blue-600"></div>
+      <div className="animate-spin rounded-full h-20 w-20 border-b-2 border-blue-600"></div>
       <p className="text-xs text-slate-500 mt-2 font-semibold">Loading...</p>
     </div>
   );
