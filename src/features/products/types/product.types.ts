bd4bcfe54eb4dc252f31c19e8f62a151116256f8
olamind <olamxii@gmail.com>
2026-07-06 20:57:44 +0300

    build(supabase): install Supabase client

---
+++ src/features/products/types/product.types.ts
@@ -0,0 +1,10 @@
+export interface Product {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  category: string;
+  images: string[];
+  creationAt: string;
+  updatedAt: string;
+}
