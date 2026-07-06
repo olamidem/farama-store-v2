8016d1ac2bc434fe76acf73291eaf6b519b03ffc
olamind <olamxii@gmail.com>
2026-07-06 14:27:18 +0300

    feat(layout): create a sidebar with the reuable components

---
+++ src/components/layout/Sidebar.tsx
@@ -0,0 +1,27 @@
+import Logo from "./Logo";
+import { navigation } from "./Navigation";
+import SidebarSection from "./SidebarSection";
+
+const Sidebar = () => {
+  return (
+    <aside className="hidden h-screen w-72 border-r border-slate-200 bg-white lg:flex lg:flex-col">
+      {/* Logo */}
+      <div className="border-b border-slate-200 p-6">
+        <Logo />
+      </div>
+
+      {/* Navigation */}
+      <nav className="flex-1 space-y-8 overflow-y-auto p-6">
+        {navigation.map((section) => (
+          <SidebarSection
+            key={section.title}
+            title={section.title}
+            items={section.items}
+          />
+        ))}
+      </nav>
+    </aside>
+  );
+};
+
+export default Sidebar;
